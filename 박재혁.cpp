4a6297c0d58570aa56b1a85c85b5bc503317a1ed
parkin14 <166615157+parkin14@users.noreply.github.com>
2024-09-06 20:51:21 +0900

    Add files via upload

---
+++ 박재혁.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+int main(int argc, char* argvs[])
+{
+	printf("씨큐브코딩은");
+	printf("내일을 위해 사과나무를 심는 것이다.");
+	return 0;  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
